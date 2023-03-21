f0d071c8bc7de8727877f5a8a6e3ecf196a31c39
giper45 <18548727+giper45@users.noreply.github.com>
2023-03-21 17:56:11 +0100

    code: refactored export of pfdif by solving characters

--- test_utils.py
+++ test_utils.py
@@ -1,8 +1,39 @@
+import os
 from utils import *
 
-def inc(x):
-    return x + 1
+def get_test_pdf():
+    return os.path.join(os.getcwd(), "tests", "test.pdf")
 
 
-def test_answer():
-    assert inc(3) == 5
+
+
+def test_pdfid():
+    expected_val = {
+    'header' : "%PDF-1.2",
+    'obj': 211,
+    'endobj': 211,
+    'stream': 59,
+    'endstream': 59,
+    'xref': 1,
+    'trailer': 1,
+    'startxref': 1,
+    'pageno': 59,
+    'Encrypt': 0,
+    'ObjStm': 0,
+    'JS': 0,
+    'JavaScript': 0,
+    'AA': 0,
+    'OpenAction': 0,
+    'AcroForm': 0,
+    'JBIG2Decode': 0,
+    'RichMedia': 0,
+    'Launch': 0,
+    'EmbeddedFile': 0,
+    'XFA': 0,
+    'URI': 0,
+    'Colors': 0
+}
+
+
+    ret = pdfid(get_test_pdf())
+    assert ret == expected_val
--- utils.py
+++ utils.py
@@ -3,6 +3,24 @@
 import os
 import subprocess
 
+var =  str(r"tr '\n' ','")
+
+STRUCTURAL_HEADER = ['header','obj','endobj','stream','endstrean','xref','trailer','startxref','pageno' ,'encrypt','ObjStm','JS','Javascript','AA','OpenAction','Acroform','JBIG2Decode','RichMedia','launch','EmbeddedFile','XFA', 'URI', 'Colors']
+
+
+
+def pfid_cmd(f):
+    return "python pdfid.py " + f 
+
+def parse_header(s):
+    return s.split(":")[1].strip()
+
+def _clean_ret(ret):
+    clean_ret = {key.replace("/", "") if key != 'header' else key: int(item) if key != 'header' else item for key, item in ret.items()}
+    # Replace name
+    clean_ret['pageno'] = clean_ret.pop('Page')
+
+    return clean_ret
 
 def pdfid(f):
     """Call the Steven script to obtain the structural features
@@ -10,8 +28,20 @@ def pdfid(f):
     Args:
         filename (str): the full path filename
     """
-    var =  str(r"tr '\n' ','")
     os.chdir('pdfid')
-    out = subprocess.getoutput("python pdfid.py " + f + " | awk '{print $2}' | tail -n +2 | "+var+"")
+    ret = {}
+    out = subprocess.getoutput(pfid_cmd(f))
+    splitted_lines = out.split("\n")[1:]
+    header = parse_header(splitted_lines[0])
+    ret['header'] = header
+    splitted_lines = splitted_lines[1:]
+    for s in splitted_lines: 
+        s_arr = s.split()
+        if len(s_arr):
+            if not s_arr[0].startswith('/Colors'):
+                ret[s_arr[0]] = s_arr[1]
+            else:
+                ret[s_arr[0]] = s_arr[3]
+
     os.chdir('../')
-    return out
+    return _clean_ret(ret)
