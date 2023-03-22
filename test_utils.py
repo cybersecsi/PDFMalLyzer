78660af0ea1377ef9dde1387be17330225f96b06
giper45 <18548727+giper45@users.noreply.github.com>
2023-03-22 10:40:40 +0100

    code: added obfuscation parameter to parse x(y) vals in columsn

--- test_utils.py
+++ test_utils.py
@@ -4,10 +4,18 @@ from utils import *
 def get_test_pdf():
     return os.path.join(os.getcwd(), "tests", "test.pdf")
 
+def get_malicious():
+    return os.path.join(os.getcwd(), "tests", "7ff7a2930d92a080ca615a8cb3f9d7d1ddec3609")
+
 
 
 def test_md5():
     assert md5sum(get_test_pdf()) == "77f3e768f0c3a240f052c711ac021a1b"
+
+def test_header():
+    assert parse_header('header: %PDF-1.5') == '%PDF-1.5'
+    assert parse_header('header: %-1.5') == 'Malformed'
+
 
 
 
@@ -25,15 +33,25 @@ def test_pdfid():
     'Encrypt': 0,
     'ObjStm': 0,
     'JS': 0,
+    'JS_Obfuscated': 0,
     'JavaScript': 0,
+    'JavaScript_Obfuscated': 0,
     'AA': 0,
+    'AA_Obfuscated': 0,
     'OpenAction': 0,
+    'OpenAction_Obfuscated': 0,
     'AcroForm': 0,
+    'AcroForm_Obfuscated': 0,
     'JBIG2Decode': 0,
+    'JBIG2Decode_Obfuscated': 0,
     'RichMedia': 0,
+    'RichMedia_Obfuscated': 0,
     'Launch': 0,
+    'Launch_Obfuscated': 0,
     'EmbeddedFile': 0,
+    'EmbeddedFile_Obfuscated': 0,
     'XFA': 0,
+    'XFA_Obfuscated': 0,
     'URI': 0,
     'Colors': 0
 }
@@ -42,3 +60,10 @@ def test_pdfid():
 
     ret = pdfid(get_test_pdf())
     assert ret == expected_val
+
+
+# With pdfs containing 1(1)
+def test_encoded():
+    expected = {'header': 'Malformed', 'obj': 8, 'endobj': 8, 'stream': 2, 'endstream': 2, 'xref': 1, 'trailer': 1, 'startxref': 1, 'Encrypt': 0, 'ObjStm': 0, 'JS': 1, 'JavaScript': 1, 'AA': 0, 'OpenAction': 1, 'AcroForm': 0, 'JBIG2Decode': 0, 'RichMedia': 0, 'Launch': 0, 'EmbeddedFile': 0, 'XFA': 0, 'URI': 0, 'Colors': 0, 'JS_Obfuscated': 1, 'JavaScript_Obfuscated': 0, 'AA_Obfuscated': 0, 'OpenAction_Obfuscated': 0, 'AcroForm_Obfuscated': 0, 'JBIG2Decode_Obfuscated': 0, 'RichMedia_Obfuscated': 0, 'Launch_Obfuscated': 0, 'EmbeddedFile_Obfuscated': 0, 'XFA_Obfuscated': 0, 'pageno': 1}
+    v = pdfid(get_malicious())
+    assert v == expected
--- utils.py
+++ utils.py
@@ -6,7 +6,15 @@ import subprocess
 
 var =  str(r"tr '\n' ','")
 
-STRUCTURAL_HEADER = ['header','obj','endobj','stream','endstrean','xref','trailer','startxref','pageno' ,'encrypt','ObjStm','JS','Javascript','AA','OpenAction','Acroform','JBIG2Decode','RichMedia','launch','EmbeddedFile','XFA', 'URI', 'Colors']
+def is_integer(n):
+    try:
+        float(n)
+    except ValueError:
+        return False
+    else:
+        return float(n).is_integer()
+
+
 
 
 def md5sum(f):
@@ -21,13 +29,47 @@ def md5sum(f):
 def pfid_cmd(f):
     return "python pdfid.py \"" + f + "\""
 
+# Valid PDF versions
+PDF_VALID_VERSIONS = [
+    '%PDF-1.0',
+    '%PDF-1.1',
+    '%PDF-1.2',
+    '%PDF-1.3',
+    '%PDF-1.4',
+    '%PDF-1.5',
+    '%PDF-1.6',
+    '%PDF-1.7',    
+    '%PDF-2.0'
+]
+
 def parse_header(s):
-    return s.split(":")[1].strip()
+    print(s.split(":"))
+    ret = s.split(":")[1].strip()
+    return ret if ret in PDF_VALID_VERSIONS else 'Malformed'
+
+def _obfuscated_fields(ret):
+    FIELDS = ['/JS', '/JavaScript', '/AA', '/OpenAction', '/AcroForm', '/JBIG2Decode', '/RichMedia', '/Launch', '/EmbeddedFile', '/XFA']
+    for f in FIELDS:
+        # Get the value
+        val = ret[f]
+        # If it is integer, simply remain the value and adds the obfuscation to 0
+        if is_integer(val):
+            ret["{}_Obfuscated".format(f)] = 0
+        # It meas that the number is in form 2(1) where 2 is the number of elements and 1 the number of obfuscations
+        else: 
+           splitted_val = val.split('(')
+           # Take 2(1) and create a new key for the obfuscation and change that of the object
+           no_objects = splitted_val[0]
+           no_obfusc  = splitted_val[1].replace(")", "")
+           ret[f] = no_objects
+           ret["{}_Obfuscated".format(f)] = no_obfusc
+
+    return ret
 
 def _clean_ret(ret):
+    ret = _obfuscated_fields(ret)
     clean_ret = {key.replace("/", "").strip() if key != 'header' else key: int(item) if key != 'header' else item.strip() for key, item in ret.items()}
     # Replace name
-    print(clean_ret)
     clean_ret['pageno'] = clean_ret.pop('Page')
 
     return clean_ret
@@ -44,6 +86,7 @@ def pdfid(f):
     ret = {}
     out = subprocess.getoutput(the_cmd)
     splitted_lines = out.split("\n")[1:]
+    print(splitted_lines)
     header = parse_header(splitted_lines[0])
     ret['header'] = header
     splitted_lines = splitted_lines[1:]
@@ -56,4 +99,5 @@ def pdfid(f):
                 ret[s_arr[0]] = s_arr[3]
 
     os.chdir('../')
-    return _clean_ret(ret)
+    clean_ret  = _clean_ret(ret)
+    return clean_ret
